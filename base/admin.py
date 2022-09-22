f2aecb9e9a248c7b56d963f2279c8503077ba984
anvarinho <anvarinho@gmail.com>
2022-09-22 16:06:49 +0600

    upgrade host heroku

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+# 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
 
 from .models import Activity, ImageModel, Region, Room, Location, Message, Sight, Tour, User, Day
 
